7be007f4428aa99890a3f24465b6774241e72d2e
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2022-04-05 19:56:58 +0530

    framework initialisation

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { UserForm } from "./views/UserForm";
+
+const userF = new UserForm(document.getElementById("root"));
+
+userF.render();
---
+++ src/models/Eventing.ts
@@ -0,0 +1,24 @@
+export type CallBack = () => void;
+
+export class Eventing {
+	events: { [key: string]: CallBack[] } = {};
+
+	on = (eventName: string, cb: CallBack): void => {
+		const handlers = this.events[eventName] || [];
+		handlers.push(cb);
+
+		this.events[eventName] = handlers;
+	};
+
+	trigger = (eventName: string): void => {
+		const handlers = this.events[eventName];
+
+		if (!handlers || handlers.length === 0) {
+			return;
+		}
+
+		handlers.forEach((cb) => {
+			cb();
+		});
+	};
+}
